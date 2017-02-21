305746b74148dfa95faaeb0d7215e27832770aa8
Miguel Santos <miguel.sl233@gmail.com>
2017-02-21 13:00:50 +0000

    Base structure and dev files

---
+++ app/js/components/Global/Menu.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export defaults class Menu extends Component {
+    render() {
+        return (
+            <div classname="Menu">
+                Main Menu
+            </div>
+        );
+    }
+}
